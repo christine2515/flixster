7a3c188ea1a64c90c08236edad2e3b69647b196e
Christine Tu <62481833+christine2515@users.noreply.github.com>
2021-04-15 20:03:47 -0700

    flix pt 2

---
+++ flix/MovieGridCell.swift
@@ -0,0 +1,14 @@
+//
+//  MovieGridCell.swift
+//  flix
+//
+//  Created by Christine Tu on 4/15/21.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
